d0d642e87f051023a044d93c188dec35d91623ca
farihanoor <fariha04noor@gmail.com>
2021-04-12 10:30:45 +0600

    gridview setup

--- flutter_grid_layout/lib/gridLayout.dart
+++ flutter_grid_layout/lib/gridLayout.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 class GridLayout extends StatelessWidget {
   List<String> events = [
@@ -15,17 +17,81 @@ class GridLayout extends StatelessWidget {
     return Scaffold(
       body: Container(
         decoration: BoxDecoration(
-         image: DecorationImage(
-          image: AssetImage(
-            "assets/bg.png",
+          image: DecorationImage(
+            image: AssetImage(
+              "assets/bg.png",
+            ),
+            fit: BoxFit.cover,
           ),
-          fit: BoxFit.cover,
-         ),
-        ), 
-        child: Container(
-          
         ),
-      ), 
+        child: Container(
+          margin: const EdgeInsets.only(top: 120.0),
+          child: GridView(
+            //physics: BouncingScrollPhysics(),
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+            children: events.map( (title) {
+                return GestureDetector(
+                  child: Card(
+                      margin: const EdgeInsets.all(20.0),
+                      child: getCardByTitle(title)),
+                  onTap: () {
+                    Fluttertoast.showToast(
+                      msg: title + "Click",
+                      toastLength: Toast.LENGTH_SHORT,
+                      gravity: ToastGravity.CENTER,
+                      backgroundColor: Colors.blue,
+                      textColor: Colors.white,
+                      fontSize: 16.0,
+                    );
+                  },
+                );
+              },
+            ).toList(),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Column getCardByTitle(String title) {
+    String img = "";
+    if (title == 'Calendar')
+      img = 'assets/calendar.png';
+    else if (title == 'Family')
+      img = 'assets/family.png';
+    else if (title == 'Friends')
+      img = 'assets/friends.png';
+    else if (title == 'Lovely')
+      img = 'assets/lovely_time.png';
+    else if (title == 'Me')
+      img = 'assets/me_time.png';
+    else if (title == 'Team') img = 'assets/team_time.png';
+
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        new Center(
+          child: Container(
+            child: new Stack(
+              children: <Widget>[
+                new Image.asset(
+                  img,
+                  width: 80.0,
+                  height: 80.0,
+                ),
+              ],
+            ),
+          ),
+        ),
+        Text(
+          title,
+          style: TextStyle(
+            fontSize: 20.0,
+            fontWeight: FontWeight.bold,
+          ),
+          textAlign: TextAlign.center,
+        ),
+      ],
     );
   }
 }
--- flutter_grid_layout/lib/main.dart
+++ flutter_grid_layout/lib/main.dart
@@ -2,7 +2,8 @@ import 'package:flutter/material.dart';
 import './gridLayout.dart';
 
 void main() => runApp(
-  MaterialApp(home:
-    GridLayout()
+  MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: GridLayout()
   ),
 );
